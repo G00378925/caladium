5f85d74e727c53b92bbff648ac383609170df5fd
Declan Kelly <84186227+G00378925@users.noreply.github.com>
2022-10-23 17:19:10 +0100

    Add loading of procmon CSV file in main.rkt

--- sandbox/main.rkt
+++ sandbox/main.rkt
@@ -9,27 +9,44 @@
 
 (define sandboxie-start-location
     (string-append (getenv "ProgramFiles") "\\Sandboxie\\Start.exe"))
-
 (define procmon-location
     (string-append (getenv "SystemDrive") "\\SysinternalsSuite\\Procmon64.exe"))
 
-(define procmon-log-file-location
-    (path->string (make-temporary-file)))
+(define procmon-pml-file-location
+    (path->string (make-temporary-file "caladium_~a.pml")))
+(define procmon-csv-file-location
+    (path->string (make-temporary-file "caladium_~a.csv")))
 
-(define-values (procmon-subprocess procmon-out procmon-in procman-err)
-    (subprocess #f #f #f procmon-location "/Minimized" "/BackingFile" procmon-log-file-location))
-
+(define-values (procmon-subprocess procmon-out procmon-in procmon-err)
+    (subprocess #f #f #f procmon-location "/Minimized" "/BackingFile" procmon-pml-file-location))
 (define-values (sandboxed-subprocess sandboxed-out sandboxed-in sandboxed-err)
     (subprocess #f #f #f sandboxie-start-location "/wait" "cmd.exe"))
 
 (define sandboxed-subprocess-pid
     (subprocess-pid sandboxed-subprocess))
-
 (subprocess-wait sandboxed-subprocess)
+
+(close-input-port sandboxed-out)
+(close-output-port sandboxed-in)
+(close-input-port sandboxed-err)
 
 (system (string-join (list procmon-location "/Terminate") " "))
 (subprocess-wait procmon-subprocess)
 
-(system (string-join (list procmon-location "/OpenLog" (string-append procmon-log-file-location ".pml")
-    "/SaveAs" (string-append procmon-log-file-location ".csv")) " "))
+(close-input-port procmon-out)
+(close-output-port procmon-in)
+(close-input-port procmon-err)
+
+(system (string-join (list procmon-location "/OpenLog" procmon-pml-file-location
+    "/SaveAs" procmon-csv-file-location) " "))
+
+(define procmon-csv-port
+    (open-input-file (string->path procmon-csv-file-location)))
+(define procmon-csv-data
+    (port->string procmon-csv-port))
+(print (string-append "procmon-csv-data size:" (number->string (string-length procmon-csv-data)) "\n"))
+(close-input-port procmon-csv-port)
+
+(delete-file (string->path procmon-pml-file-location))
+(delete-file (string->path procmon-csv-file-location))
 
