2a8a3d41e579843b2436e1de57f5a2f35c6eab8b
Declan Kelly <84186227+G00378925@users.noreply.github.com>
2023-01-17 22:05:40 +0000

    Add record deletion test

--- server/tests/setup.py
+++ server/tests/setup.py
@@ -6,7 +6,7 @@
 #  Copyright © 2023 Declan Kelly. All rights reserved.
 #
 
-import json, os, sys
+import json, os, sys, unittest
 
 import requests
 
@@ -16,8 +16,28 @@ if not os.environ.get("CALADIUM_SERVER_ADDRESS", None):
 
 CALADIUM_SERVER_ADDRESS = os.environ["CALADIUM_SERVER_ADDRESS"]
 
-def get_http_headers(server_address, username, password):
-    headers = {"username": username, "password": password}
-    token = json.loads(requests.post(f"http://{server_address}/api/login", headers=headers).text)["Authorisation"]
-    return {"Authorisation": token}
+class EndPointTestCase(unittest.TestCase):
+    def get_http_headers(self, username, password):
+        headers = {"username": username, "password": password}
+        token = json.loads(requests.post(f"http://{CALADIUM_SERVER_ADDRESS}/api/login", headers=headers).text)["Authorisation"]
+        return {"Authorisation": token}
+
+    def get_all_records(self):
+        return json.loads(requests.get(f"http://{CALADIUM_SERVER_ADDRESS}{self.endpoint}").text)
+
+    def create_new_record(self, new_record):
+        requests.post(f"http://{CALADIUM_SERVER_ADDRESS}{self.endpoint}", data=new_record)
+
+    def setUp(self, endpoint, example_record):
+        self.endpoint = endpoint
+        self.example_record = example_record
+        self.http_headers = self.get_http_headers("root", "root")
+
+        self.test_delete_all_records()
+
+        for _ in range(100): self.create_new_record(self.example_record)
+
+    def test_delete_all_records(self):
+        requests.delete(f"http://{CALADIUM_SERVER_ADDRESS}{self.endpoint}")
+        self.assertEqual(len(self.get_all_records()), 0)
 
