4626d98288b6571a7f3e1d2b26a07a12e2de3888
Declan Kelly <84186227+G00378925@users.noreply.github.com>
2023-01-30 19:02:02 +0000

    Can analyse syscalls in parallel

--- sandbox/syscall_analysis.py
+++ sandbox/syscall_analysis.py
@@ -6,12 +6,14 @@
 #  Copyright © 2023 Declan Kelly. All rights reserved.
 #
 
-import json, struct, sys
+import collections, json, struct, sys, threading
 
 def output_json(json_obj):
+    globals()["stdout_lock"].acquire()
     json_obj_bytes = json.dumps(json_obj).encode()
     sys.stdout.buffer.write(struct.pack(">I", len(json_obj_bytes)))
     sys.stdout.buffer.write(json_obj_bytes)
+    globals()["stdout_lock"].release()
 
 def send_message(text):
     output_json({"type": "message", "text": text})
@@ -22,21 +24,42 @@ def send_progress(value):
 def send_state(state):
     output_json({"type": "state", "state": state})
 
-def get_file_lines(file_location):
-    with open(file_location) as file_handle:
-        return file_handle.readlines()
+def inc_analysis_count():
+    globals()["count_lock"].acquire()
+    globals()["analysis_count"] += 1
+    globals()["count_lock"].release()
+
+def fetch_next_syscall():
+    globals()["deque_lock"].acquire()
+    syscall_deque = globals()["syscall_deque"]
+    syscall = syscall_deque.pop() if len(syscall_deque) > 1 else None
+    globals()["deque_lock"].release()
+    return syscall
+
+def analysis_thread_func(malicious_pattern_list):
+    while syscall := fetch_next_syscall():
+        for pattern in malicious_pattern_list:
+            if pattern in syscall:
+                send_message(pattern)
+        inc_analysis_count()
 
 def main(argv):
+    def get_file_lines(file_location):
+        with open(file_location) as file_handle:
+            return file_handle.readlines()
+
     if len(argv) < 2: return
     syscall_file_location, malicious_pattern_file_location = argv
-
-    send_message("syscall_analysis.py started")
 
     syscall_list = get_file_lines(syscall_file_location)
     malicious_pattern_list = get_file_lines(malicious_pattern_file_location)
 
-    for pattern in malicious_pattern_list:
-        send_message(pattern)
+    globals()["count_lock"], globals()["deque_lock"], globals()["stdout_lock"] = [threading.Lock()] * 3
+    globals()["analysis_count"], globals()["syscall_deque"] = 0, syscall_list
+
+    for _ in range(10): threading.Thread(target=analysis_thread_func, args=[malicious_pattern_list]).start()
+    while globals()["analysis_count"] < len(syscall_list): ...
+    sys.exit(0)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
