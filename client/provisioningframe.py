2b0ece8d2e78341903b1b68aeeefcd0a625550ec
Declan Kelly <84186227+G00378925@users.noreply.github.com>
2023-02-05 22:29:57 +0000

    Detect malformed provisioning tokens

--- client/provisioningframe.py
+++ client/provisioningframe.py
@@ -6,14 +6,14 @@
 #  Copyright © 2022 Declan Kelly. All rights reserved.
 #
 
-import base64, tkinter, tkinter.filedialog, tkinter.ttk
+import base64, json, tkinter, tkinter.filedialog, tkinter.ttk
 
 import provisioning
 
 class ProvisioningFrame(tkinter.ttk.Frame):
     def __init__(self, main_window, provisioning_complete, caladium_appdata_dir):
         super().__init__(main_window)
-        self.provisioning_complete = provisioning_complete
+        self.provisioning_complete, self.caladium_appdata_dir = provisioning_complete, caladium_appdata_dir
 
         self.token_entry_box = tkinter.Entry(self)
         self.token_entry_box.pack()
@@ -28,5 +28,13 @@ class ProvisioningFrame(tkinter.ttk.Frame):
                 provisioning_complete()
 
     def _provision_token(self):
-        ...
+        try:
+            token_obj = json.loads(base64.b64decode(self.token_entry_box["text"]).decode("utf-8"))
+            provisioning.save_config(self.caladium_appdata_dir, token_obj)
+
+            globals()["config"] = provisioning.load_config(caladium_appdata_dir)
+            if provisioning.test_server_connection(globals()["config"]):
+                provisioning_complete()
+        except (json.decoder.JSONDecodeError):
+            tkinter.messagebox.showerror("Error", "Provision token is malformed.")
 
